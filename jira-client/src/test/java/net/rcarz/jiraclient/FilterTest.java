aebd77aeec8765ed707973a4e41e8def7a015792
Administrator <Administrator@PC-201709041343>
2019-04-16 15:19:48 +0800

    上传jira client

---
+++ jira-client/src/test/java/net/rcarz/jiraclient/FilterTest.java
@@ -0,0 +1,21 @@
+package net.rcarz.jiraclient;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test cases for stuff relating to filters.
+ */
+public class FilterTest {
+
+	@Test
+	public void testGetFilter() throws JiraException {
+		
+		JiraClient jira = new JiraClient("https://jira.shishike.com/", null);
+
+	
+	}
+
+}
